b2ed996aa7a44f85adf5e3b5d39d524277e85e14
Dana Simmons <dana@simmons-audio.com>
2019-04-08 15:31:35 -0400

    search function is working

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,14 +5,44 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  
+    state = {
+       pokemon: [],
+       searchTerm: ""
+    }
+  
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({pokemon: data})
+    })
+  }
+
+  handleSearch = (e, {value}) => {
+    this.setState({searchTerm: value})
+  }
+  
+  filterPokemon(){
+  
+    const filterString = this.state.searchTerm
+    const filteredPokemon = this.state.pokemon.filter((pokemon)=>{
+      return pokemon.name.includes(filterString)
+    })
+    console.log('filteredString :', filterString);
+    console.log('filteredPokemon :', filteredPokemon);
+    //return array of pokemon
+    return filteredPokemon
+  }
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={_.debounce(this.handleSearch, 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.filterPokemon()} />
         <br />
         <PokemonForm />
       </div>
